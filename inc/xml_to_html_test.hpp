64d5af6207c176289fdf16ee845f82dd2a866ed9
justin-is-afk <jlgodson.eng@gmail.com>
2020-12-15 16:13:13 -0500

    Add some more comments, and modify main to instead run the included Gtest unit tests

--- inc/xml_to_html_test.hpp
+++ inc/xml_to_html_test.hpp
@@ -3,7 +3,15 @@
 
 #include <string>
 
+//! testConv is the main test entry point for converting XML data to a HTML table
+//! \param[in] xmlFileName the file name of the input XML file
+//! \param[in] htmlFileName the file name of the output HTML file
+//! \param[in] dataSeriesName the name of the data series to parse and convert
+//! ret True on success, false otherwise
 bool testConv(std::string xmlFileName, std::string htmlFileName, std::string dataSeriesName);
+
+// Justin's Notes:
+// These are just some basic unit test cases, more could and likely should be added
 bool testValidXML();
 bool testMalformedXML();
 bool testEmptyXML();
--- src/main.cpp
+++ src/main.cpp
@@ -7,26 +7,8 @@ using namespace std;
 
 int main(int argc, char **argv)
 {
-	assert(argc >= 2);
-	const string fileName = argv[1];
-	LOG_F(INFO, "Begin XML to HTML Table conversion of \"%s\"", fileName.c_str());
-
-	// Justin's Notes:
-	// As the exercise description called for an executable that would take in a filename,
-	// I've "hard-coded" these values. They should however be taked as inputs, just as
-	// the input XML file name is.
-	const string htmlFileName = "data/table.html";
-	const string dataSeriesName = "CATALOG";
-	testConv(fileName, htmlFileName, dataSeriesName);
-
-	// Justin's Notes:
-	// As the exercise called for "engineering best practices" I thought it appropriate to
-	// include some unit tests, as well as a logger. The logger simply outputs to the console
-	// for simplicity, but could be set to instead log to a file. There should be a pull request 
-	// open in the github repo to change `main` to instead run the google test unit tests that
-	// I've included in `xml_to_html_test.cpp`.
-	//LOG_F(INFO, "Begin XML to HTML Table testing...");
-	//testing::InitGoogleTest(&argc, argv);
-	//return RUN_ALL_TESTS();
+	LOG_F(INFO, "Begin XML to HTML Table testing...");
+	testing::InitGoogleTest(&argc, argv);
+	return RUN_ALL_TESTS();
 	return 0;
 }
