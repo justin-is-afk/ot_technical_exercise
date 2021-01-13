b0dfc9501d3c82fbc405506e5a472dd4a1b59331
justin-is-afk <jlgodson.eng@gmail.com>
2021-01-13 09:56:56 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 sudo apt-get install build-essential
+sudo apt-get install cmake
 sudo apt-get install libgtest-dev
 cd /usr/src/googletest/googletest
-sudo cmake
-sudo make install
+sudo cmake CMakelists.txt
+sudo make
